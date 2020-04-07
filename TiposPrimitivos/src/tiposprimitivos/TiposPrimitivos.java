64ba527718538684482a581e5afe29d114a65868
Gustavo Rodrigues Mietlicki <gustavo.mietlicki@deal.br>
2020-04-07 11:07:10 -0300

    Terceiro commit, tipos primitivos e calculadora com Swing

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author gustavo.mietlicki
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome = "Gustavo";
+        float nota = 8.5f;
+        System.out.format("O nome do aluno é : %s e a nota é: %.2f \n", nome , nota);
+    }
+    
+}
