02aea27cec66db5a1007a89ba6b8af32ab97043b
Gustavo Rodrigues Mietlicki <gustavo.mietlicki@deal.br>
2020-04-10 12:44:43 -0300

    Exercicios com vetores

---
+++ ExercicioVetor/src/classes/TelaVetor.java
@@ -0,0 +1,173 @@
+package classes;
+
+import java.util.Arrays;
+import javax.swing.DefaultListModel;
+
+public class TelaVetor extends javax.swing.JFrame {
+    
+    int vetor[] = new int[5];
+    DefaultListModel lista = new DefaultListModel();
+    int selecionado = 0;
+   
+    public TelaVetor() {
+        initComponents();
+        for(int c = 0; c < vetor.length; c++){
+            lista.addElement(vetor[c]); 
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txtNum = new javax.swing.JSpinner();
+        btnAdd = new javax.swing.JButton();
+        btnRemove = new javax.swing.JButton();
+        btnOrdem = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        lblSelecionado = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lstVetor = new javax.swing.JList<>();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        txtNum.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtNumMouseClicked(evt);
+            }
+        });
+
+        btnAdd.setText("Adicionar");
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
+
+        btnRemove.setText("Remover");
+        btnRemove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRemoveActionPerformed(evt);
+            }
+        });
+
+        btnOrdem.setText("Ordenar");
+        btnOrdem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOrdemActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Vetor");
+
+        lblSelecionado.setText("[ 0 ]");
+
+        lstVetor.setModel(lista);
+        lstVetor.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lstVetorMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(lstVetor);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(txtNum, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAdd)
+                    .addComponent(btnRemove)
+                    .addComponent(btnOrdem))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(39, 39, 39)
+                        .addComponent(jLabel1)
+                        .addGap(31, 31, 31)
+                        .addComponent(lblSelecionado, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(26, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAdd)
+                    .addComponent(jLabel1)
+                    .addComponent(lblSelecionado))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnRemove)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnOrdem)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        vetor[selecionado] = Integer.parseInt(txtNum.getValue().toString());
+        lista.removeAllElements();
+        for(int c = 0; c < vetor.length; c++){
+            lista.addElement(vetor[c]);
+        }
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnRemoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveActionPerformed
+        vetor[selecionado] = 0;
+        lista.removeAllElements();
+        for(int c = 0; c < vetor.length; c++){
+            lista.addElement(vetor[c]);
+        }
+        
+    }//GEN-LAST:event_btnRemoveActionPerformed
+
+    private void btnOrdemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOrdemActionPerformed
+        Arrays.sort(vetor);
+        lista.removeAllElements();
+        for(int c = 0; c < vetor.length; c++){
+            lista.addElement(vetor[c]);
+        }
+    }//GEN-LAST:event_btnOrdemActionPerformed
+
+    private void lstVetorMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lstVetorMouseClicked
+        selecionado = lstVetor.getSelectedIndex();
+        lblSelecionado.setText("[ "+selecionado+" ]");
+
+    }//GEN-LAST:event_lstVetorMouseClicked
+
+    private void txtNumMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtNumMouseClicked
+
+    }//GEN-LAST:event_txtNumMouseClicked
+
+    public static void main(String args[]) {
+      
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaVetor().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnOrdem;
+    private javax.swing.JButton btnRemove;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblSelecionado;
+    private javax.swing.JList<String> lstVetor;
+    private javax.swing.JSpinner txtNum;
+    // End of variables declaration//GEN-END:variables
+}
